b25bbc69e460e1aa5d12ac2aaf95e5a0086ebe8c
kozutyn <137905127+kozutyn@users.noreply.github.com>
2025-06-08 20:36:05 +0200

    Update homework2.py

--- homework2.py
+++ homework2.py
@@ -2,13 +2,12 @@ import re
 from typing import Callable
 
 def generator_numbers(text: str):
-    matches = re.findall(r' (?P<number>\d+\.\d+) ', text)
-    for number in matches:
-        yield float(number)
+    for match in re.findall(r' (?P<number>\d+\.\d+) ', text):
+        yield float(match)
 
 def sum_profit(text: str, func: Callable):
     return sum(func(text))
 
-text = "Общий доход работника состоит из нескольких частей: 1000.01 как основной доход, дополненный дополнительными поступлениями 27.45 и 324.00 долларов."
+text = "Общий доход работника состоит из нескольких частей: 1000.01 как основной доход, дополненный дополнительными поступлениями 27.45 и 324.00 долларов." 
 total_income = sum_profit(text, generator_numbers)
 print(f"Общий доход: {total_income}")
