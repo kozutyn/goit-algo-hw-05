3a9695c53c47e85c8e690bd2623a7115ada2c7ff
kozutyn <137905127+kozutyn@users.noreply.github.com>
2025-06-08 21:38:07 +0200

    main.py

--- main.py
+++ main.py
@@ -2,24 +2,15 @@ import re
 from typing import Callable, Generator
 
 def generator_numbers(text: str) -> Generator[float, None, None]:
-    """
-    Генератор, извлекающий действительные числа из текста.
-    Числа считаются только если они окружены пробелами с обеих сторон.
-    """
-    pattern = r'\s\d+\.\d+\s'
-    for number in re.findall(pattern, text):
-        yield float(number)
+    pattern = r' (?P<number>\d+\.\d+) '
+    for match in re.finditer(pattern, text):
+        yield float(match.group('number'))
 
 def sum_profit(text: str, func: Callable[[str], Generator[float, None, None]]) -> float:
-    """
-    Вычисляет общую сумму чисел, найденных в тексте с помощью генератора.
-    """
     return sum(func(text))
 
 if __name__ == "__main__":
-    text = (
-        "Общий доход работника состоит из нескольких частей: 1000.01 как основной доход, "
-        "дополненный дополнительными поступлениями 27.45 и 324.00 долларов."
-    )
+    text = ("Общий доход работника состоит из нескольких частей: 1000.01 как основной доход, "
+            "дополненный дополнительными поступлениями 27.45 и 324.00 долларов.")
     total_income = sum_profit(text, generator_numbers)
     print(f"Общий доход: {total_income}")
